931dfda13049a71aed1d78c5c0b02348b54cb683
Nixon <limnixon03292001@gmail.com>
2022-10-05 22:29:53 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const userRoutes = require('./routes/userRoutes');
 const messagesRoutes = require('./routes/messagesRoutes');
