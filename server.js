be2f7441d71275015526d7f83bf14d91f6f983aa
Nixon <limnixon03292001@gmail.com>
2022-12-23 11:00:17 +0800

    update: deployment change

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.use('/api/marketplace', marketplaceRoutes);
 
 //fixed when refresh cannot get the route
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    // app.use(express.static(path.join(__dirname, "client/build")));
+
+    app.use(express.static("client/build"));
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
 }
 
 // Function to start the server
